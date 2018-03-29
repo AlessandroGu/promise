ccff46ebf22788b94364a281a9b78e59bccdc3f3
AlessandroGu <924291390@qq.com>
2018-03-29 13:56:08 +0800

    third commit

--- promise.js
+++ promise.js
@@ -77,3 +77,48 @@ getJson('/post/1.json').then(function(post) {
 }, function funB(err){
     console.log('rejected:', error);
 })
+
+//promise产生的错误具有冒泡性质，会一直向后传递，直到被catch(捕获)为止，一般来说，then后面处理reject状态的回调函数最好不要定义，直接写一个catch比较好.
+//catch内部还能抛出异常，但是一定要用catch来捕获，不然直接跳过异常，执行完毕。
+
+//在ES2018中，引入了finally方法，它不接受任何参数，与promise的状态无关，不管最终promise是resolve还是reject，它都会执行，finally方法总是会返回原来的值。
+promise
+.finally(() => {
+    // do something;
+})
+//上面和下面的效果是一样的，用finally的话代码可以更简洁。
+promise
+.then(
+    result => {
+        return result;
+    },
+    error => {
+        return error;
+    }
+);
+
+//剖析一下finally的实现原理
+Promise.prototype.finally = function(callback) {
+    let p = this.constructor;
+    return this.then(
+        value => p.resolve(callback()).then(() => value),
+        reason => p.resolve(callback()).then(() => {throw reason})
+    )
+}
+
+//promise.all方法：
+const p = Promise.all([p1, p2, p3]);
+//p的状态由p1,p2,p3决定，只有3个都是resolve的时候，p才会变成resolve,此时p1,p2,p3的返回值组成一个数组传递给p的回调。
+//只要其中一个的状态为reject，那么p的状态就是reject，此时第一个被reject的实例的返回值，会传递给p的回调。
+
+// 生成一个Promise对象的数组
+const promises = [2, 3, 5, 7, 11, 13].map(function (id) {
+  return getJSON('/post/' + id + ".json");
+});
+
+Promise.all(promises).then(function (posts) {
+  // do something
+}).catch(function(reason){
+  // do something
+});
+//此时的promise是包含6个promise的实例，只有当这6个实例的状态全部变成resolve或者其中一个变为reject的时候，才会调用all后面的回调。
