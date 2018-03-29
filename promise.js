54c8eb1e7f8e550a2d9505de0a9ac3f4e597e03e
AlessandroGu <924291390@qq.com>
2018-03-29 15:16:49 +0800

    fourth commit

--- promise.js
+++ promise.js
@@ -122,3 +122,72 @@ Promise.all(promises).then(function (posts) {
   // do something
 });
 //此时的promise是包含6个promise的实例，只有当这6个实例的状态全部变成resolve或者其中一个变为reject的时候，才会调用all后面的回调。
+
+
+//注意：假如promise作为参数，自己定义了catch方法，那么它一旦被rejected，并不会触发Pormise.all()的catch方法
+const p1 = new Promise((resolve, reject) => {
+  resolve('hello');
+})
+.then(result => result)
+.catch(e => e);
+
+const p2 = new Promise((resolve, reject) => {
+  throw new Error('报错了');
+})
+.then(result => result)
+.catch(e => e);
+
+Promise.all([p1, p2])
+.then(result => console.log(result))
+.catch(e => console.log(e));
+// ["hello", Error: 报错了]
+
+
+//let p = Promise.race([p1,p2,p3])只要其中一个改变状态，那么p的状态就会随之改变，第一个改变状态的promise的返回值，会传递给p的回调。
+//race方法需要与下文的Promise.resolve结合起来看。
+
+//Promise.resolve
+//(1).参数是一个promise实例，那么就直接返回这个实例
+//(2).参数是一个thenable对象，转化完成以后，立即执行.then方法
+//(3).参数不是具有then方法的对象，或许压根不是对象，直接变为resolved，参数传递给回调，输出
+//(4).不带有任何参数是合法的，直接返回一个resolved的promise对象。需要注意的是，立即resolve的 Promise 对象，是在本轮“事件循环”（event loop）的结束时，而不是在下一轮“事件循环”的开始时。
+
+//Promise.reject
+//注意，Promise.reject()方法的参数，会原封不动地作为reject的理由，变成后续方法的参数。这一点与Promise.resolve方法不一致。
+const thenable = {
+  then(resolve, reject) {
+    reject('出错了');
+  }
+};
+
+Promise.reject(thenable)
+.catch(e => {
+  console.log(e === thenable)
+})
+// true
+// 上面代码中，Promise.reject方法的参数是一个thenable对象，执行以后，后面catch方法的参数不是reject抛出的“出错了”这个字符串，而是thenable对象。
+
+
+//Promise.try
+//假如把所有的函数都放在.then后面的回调中去执行的话，那么事件都会被放到本轮事件循环的末尾执行，无法区分同步函数与异步函数的执行顺序。
+//ES6官网介绍了很多种写法，声明一个函数，然后用async函数去代替，但是有很多不完美的地方，用promise.try更好一点。
+const f = () => console.log('now');
+Promise.try(f);
+console.log('next');
+// now
+// next
+//如果想用then管理好流程，最好用try包装一下，可以更好的管理异常
+function getUsername(userId) {
+  return database.users.get({id: userId})
+  .then(function(user) {
+    return user.name;
+  });
+}
+//database.users.get()返回的是一个promise对象，可以用catch来捕获异常，但是可能还会抛出同步错误，那就不得不用try...catch
+//这样写会显得很笨拙，完全可以写成下面的这种形式:
+Promise.try(database.user.get({id: userId}))
+.then(...)
+.catch(...)
+
+
+//关于promise的总结就到这里，总结的不是很全面，很多点都漏了，等后期有时间再把遗漏的弥补上来。
